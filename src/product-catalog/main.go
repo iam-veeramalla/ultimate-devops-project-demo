6cf95597e9b73c76347b1714f11b83fa54288b30
PranavKumar <pranav.shahi@yahoo.com>
2025-09-28 18:17:44 +0530

    chore: verify github actions

--- src/product-catalog/main.go
+++ src/product-catalog/main.go
@@ -316,12 +316,7 @@ func createClient(ctx context.Context, svcAddr string) (*grpc.ClientConn, error)
 	)
 }
 
-func createClient(ctx context.Context, svcAddr string) (*grpc.ClientConn, error) {
-	return grpc.DialContext(ctx, svcAddr,
-		grpc.WithTransportCredentials(insecure.NewCredentials()),
-		grpc.WithStatsHandler(otelgrpc.NewClientHandler()),
-	)
-}
+
 
 
 
