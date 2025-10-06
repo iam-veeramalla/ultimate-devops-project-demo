2a9db85b48c191f3a657a004b0acb5a98c882f01
amit <akworld24@gmail.com>
2025-10-06 12:07:02 +0530

    chore: verify github actions

--- src/product-catalog/main.go
+++ src/product-catalog/main.go
@@ -313,8 +313,14 @@ func createClient(ctx context.Context, svcAddr string) (*grpc.ClientConn, error)
 	return grpc.DialContext(ctx, svcAddr,
 		grpc.WithTransportCredentials(insecure.NewCredentials()),
 		grpc.WithStatsHandler(otelgrpc.NewClientHandler()),
+
+
 	)
 }
+
+
+
+
 
 
 
